9fe89ca30b1c58b36668e87bf3c7eeb8c42d3eb9
Akshada018 <akshada944@gmail.com>
2023-04-05 15:12:43 +0530

    Changes in refconfig file.

--- springcore/src/main/java/com/springcore/ref/TestRef.java
+++ springcore/src/main/java/com/springcore/ref/TestRef.java
@@ -6,11 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestRef {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"com/springcore/ref/refconfig.xml");
-	A a1 = (A)context.getBean("aref");
-	System.out.println(a1.getX());
-	System.out.println(a1.getB().getY());
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/ref/refconfig.xml");
+		A a1 = (A) context.getBean("aref");
+		System.out.println(a1.getX());
+		System.out.println(a1.getB().getY());
+		System.out.println(a1);
 	}
 
 }
