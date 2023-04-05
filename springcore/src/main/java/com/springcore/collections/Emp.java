6d424cd4e8d66dd7fad5fe4792c82c802eaeadc4
Akshada018 <akshada944@gmail.com>
2023-04-05 12:09:19 +0530

    SpringCore Collection Example.

---
+++ springcore/src/main/java/com/springcore/collections/Emp.java
@@ -0,0 +1,64 @@
+package com.springcore.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Emp {
+
+	private String name;
+	private Set<String> Phones;
+	private List<String> addresses;
+	private Map<String, String> courses;
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Emp(String name, Set<String> phones, List<String> addresses, Map<String, String> courses) {
+		super();
+		this.name = name;
+		Phones = phones;
+		this.addresses = addresses;
+		this.courses = courses;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [name=" + name + ", Phones=" + Phones + ", addresses=" + addresses + ", courses=" + courses + "]";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<String> getPhones() {
+		return Phones;
+	}
+
+	public void setPhones(Set<String> phones) {
+		Phones = phones;
+	}
+
+	public List<String> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<String> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Map<String, String> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(Map<String, String> courses) {
+		this.courses = courses;
+	}
+
+}
